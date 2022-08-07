2fce5a4b00f47bd37c9c130a527f7ed7227d59b2
yony1996 <yonyvenegas@gmail.com>
2022-08-06 21:07:55 -0500

    cambios en api appoiment

--- app/Http/Controllers/Api/AppoimentController.php
+++ app/Http/Controllers/Api/AppoimentController.php
@@ -6,15 +6,20 @@ use App\Http\Controllers\Controller;
 use App\Models\Appoiment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AppoimentController extends Controller
 {
     public function index()
     {
         $user = Auth::guard('api')->user()->patient;
-        $appoiments = Appoiment::Appoiments();
-        $data = compact('appoiments');
-        return $data;
+        $appoiments =  Appoiment::join('users', 'users.id', '=', 'appoiments.user_id')
+            ->join('exams', 'exams.id', '=', 'appoiments.exam_id')
+            ->where('user_id', '=', Auth::user()->id)
+            ->select('exams.name', 'appoiments.status','appoiments.scheduled_date','appoiments.scheduled_time')
+            ->get();
+
+        return  $appoiments;
     }
 
     public function store(Request $request)
